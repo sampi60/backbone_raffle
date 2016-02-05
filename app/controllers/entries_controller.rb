0408ab2e375e1a020a24724f6765b379b5521b6c
Piotr Sampławski <sampi@gumed.edu.pl>
2016-02-05 14:08:31 +0100

    Add permitted_params method to entries_controller.rb

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -10,14 +10,21 @@ class EntriesController < ApplicationController
   end
 
   def create
-    respond_with Entry.create(params[:entry])
+    respond_with Entry.create(permitted_params)
   end
 
   def update
-    respond_with Entry.create(params[:id], params[:entry])
+    respond_with Entry.create(params[:id], permitted_params)
   end
 
   def destroy
     respond_with Entry.destroy(params[:id])
+  end
+
+
+  private
+
+  def permitted_params
+    params.require(:entry).permit(:id, :name, :winner, :created_at, :updated_at)
   end
 end
